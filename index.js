f8ca3e72e166b6f98e3054af4a803d5f4183082b
Vadym Lavorchuk <lavorchukvadim@gmail.com>
2018-07-24 15:54:51 +0300

    30-40%

--- index.js
+++ index.js
@@ -10,11 +10,10 @@ let savedData = [];
 
 let basePropertyUrl = 'https://www.olx.ua/';
 
-
 let properties = new osmosis
   .get(basePropertyUrl)
   // .config('proxy', '159.224.15.231:8080')
-  .delay(3000)
+  .delay(2000)
   .find('#searchmain-container .maincategories .maincategories-list:nth-child(1) .li:nth-child(2) .item')
   // .find('#topLink li.visible:nth-child(2)')
   .follow('a')
@@ -22,12 +21,12 @@ let properties = new osmosis
   .follow('a')
   // .find('#locationLinks .locationlinks')
   // .follow('a')
-  .delay(3000)
+  .delay(2000)
   .paginate('.pager.rel.clr .fbold.next.abs.large a[href]', 2)
-  .delay(3000)
+  .delay(2000)
   .find('#offers_table td.offer')
   .follow('a.detailsLink.link')
-  .delay(3000)
+  .delay(8000)
   .set({
     'title': '#offerdescription .offer-titlebox h1',
     'price': '#offerbox .price-label strong',
@@ -48,7 +47,8 @@ let properties = new osmosis
     parsedUrl.uri = urlTemp;
     parsedUrl.url = urlTemp;
     Object.keys(document.cookies).forEach((item, key) => {
-      if (key == 'PHPSESSID') {
+      if (item == 'PHPSESSID') {
+        // cookies += item + '=' + document.cookies[item] + '; ';
         cookies += item + '=' + document.cookies[item];
       }
     });
@@ -56,18 +56,18 @@ let properties = new osmosis
     // cookies = cookies.substring(0, cookies.length - 2);
 
     parsedUrl.headers = {
-      // 'Accept': '*/*',
-      // 'Accept-Encoding': 'gzip, deflate, br',
-      // 'Accept-Language': 'en-GB,en;q=0.9',
-      // 'Cache-Control': 'no-cache',
-      // 'Connection': 'keep-alive',
+      'Accept': '*/*',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Accept-Language': 'en-GB,en;q=0.9',
+      'Cache-Control': 'no-cache',
+      'Connection': 'keep-alive',
       'Cookie': cookies,
-      // 'Host': document.__location.host,
-      // 'Pragma': 'no-cache',
-      'Referer': document.__location.origin + document.__location.pathname,
-      // 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
-      // 'X-NewRelic-ID': xpid,
-      // 'X-Requested-With': 'XMLHttpRequest',
+      'Host': document.__location.host,
+      'Pragma': 'no-cache',
+      'Referer': document.__location.href,
+      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+      'X-NewRelic-ID': xpid,
+      'X-Requested-With': 'XMLHttpRequest',
     };
 
     rp(parsedUrl, (error, response, body) => {
@@ -79,7 +79,7 @@ let properties = new osmosis
       }
     });
   })
-  .delay(5000)
+  .delay(1000)
   .data((data) => {
     if (data.phone) {
       success++;
