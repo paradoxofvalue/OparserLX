774a7ea941c0b8b1a5e3420b710f56796b8bf712
Vadym Lavorchuk <vadym.lavorchuk.avezorsoft@gmail.com>
2018-08-10 18:29:18 +0300

    rp to lokmap

--- index.js
+++ index.js
@@ -17,9 +17,39 @@ var tunelingAgent = tunnel.httpsOverHttp({
   }
 });
 
+let areas = [
+  { name: 'Винницкая область', url: 'vin' },
+  { name: 'Волынская область', url: 'vol' },
+  { name: 'Днепропетровская область', url: 'dnp' },
+  { name: 'Донецкая область', url: 'don' },
+  { name: 'Житомирская область', url: 'zht' },
+  { name: 'Закарпатская область', url: 'zak' },
+  { name: 'Запорожская область', url: 'zap' },
+  { name: 'Ивано-Франковская область', url: 'if' },
+  { name: 'Киевская область', url: 'ko' },
+  { name: 'Кировоградская область', url: 'kir' },
+  { name: 'Крым (АРК)', url: 'cri' },
+  { name: 'Луганская область', url: 'lug' },
+  { name: 'Львовская область', url: 'lv' },
+  { name: 'Николаевская область', url: 'nik' },
+  { name: 'Одесская область', url: 'od' },
+  { name: 'Полтавская область', url: 'pol' },
+  { name: 'Ровенская область', url: 'rov' },
+  { name: 'Сумская область', url: 'sum' },
+  { name: 'Тернопольская область', url: 'ter' },
+  { name: 'Харьковская область', url: 'kha' },
+  { name: 'Херсонская область', url: 'khe' },
+  { name: 'Хмельницкая область', url: 'khm' },
+  { name: 'Черкасская область', url: 'chk' },
+  { name: 'Черниговская область', url: 'chn' },
+  { name: 'Черновицкая область', url: 'chv' },
+];
 
-var startURL = 'https://www.olx.ua/nedvizhimost/zht/';
+var startURL = 'https://www.olx.ua/nedvizhimost/';
 var results = [];
+
+var a = tress(performAreas);
+a.drain = doneAreas;
 
 var p = tress(performPaginate);
 p.drain = donePaginate;
@@ -33,9 +63,21 @@ start();
 let count = 0;
 
 function start() {
-  needle.get(startURL, { agent: tunelingAgent }, function (err, res) {
+  areas.forEach(item => {
+    let url = startURL+item.url+'/';
+    needle.get(url, { agent: tunelingAgent }, function (err, res) {
+      if (err) throw err;
+      console.log(url);
+      a.push(resolve(startURL, url));
+    });
+  });
+  
+}
+
+function performAreas(url, cb) {
+  needle.get(url, { agent: tunelingAgent }, function (err, res) {
     if (err) throw err;
-    console.log(startURL);
+    console.log(url);
     var $ = cheerio.load(res.body);
     // get ads from first page
     $('#offers_table .offer .title-cell a.detailsLink').each(function () {
@@ -44,9 +86,10 @@ function start() {
     // get pages
     $('.content .pager .item a').each(function () {
       if ($(this).text().trim().length < 2) {
-        p.push(resolve(startURL, $(this).attr('href')));
+        p.push(resolve(url, $(this).attr('href')));
       }
     });
+    cb();
   });
 }
 
@@ -72,6 +115,9 @@ function performAd(url, cb) {
       address = $('#offerdescription .offer-titlebox .show-map-link strong').text().trim(),
       description = $('#offerdescription #textContent').text().trim(),
       name = $('#offerbox .offer-sidebar__box .offer-user__details h4 a').text().trim(),
+      lat = $('#mapcontainer').attr('data-lat'),
+      lon = $('#mapcontainer').attr('data-lon'),
+      rad = $('#mapcontainer').attr('data-rad'),
       attributes = [],
       images = [];
 
@@ -101,9 +147,18 @@ function performAd(url, cb) {
           'description': description,
           'attributes': attributes,
           'name': name,
+          'lat': lat,
+          'lon': lon,
+          'rad': rad,
           'images': images,
         });
-        cb();
+        saveAd(results[results.length - 1]).then(
+          result => {
+            debugger;
+            cb();
+          }
+        )
+        
       },
       error => {
         phone = false;
@@ -119,13 +174,23 @@ function done() {
   console.log(results);
 }
 
+function doneAreas() {
+  // тут что-то делаем с массивом результатов results
+  // например, выводит в консоль на этапе тестирования
+  debugger;
+  console.log(results);
+  fs.writeFile('data.json', JSON.stringify(results, null, 4), function (err) {
+    if (err) console.error(err);
+    else console.log('Data Saved to data.json file');
+  })
+}
+
 function donePaginate() {
   // тут что-то делаем с массивом результатов results
   // например, выводит в консоль на этапе тестирования
   debugger;
   console.log(results);
 }
-
 
 function scrapePhone(res, url) {
   let phoneToken = res.body.split("phoneToken")[1].split("'")[1].split("'")[0],
@@ -175,7 +240,6 @@ function scrapePhone(res, url) {
   });
 }
 
-
 function IsJsonString(string) {
   try {
     JSON.parse(string);
@@ -183,4 +247,40 @@ function IsJsonString(string) {
     return false;
   }
   return true;
+}
+
+function saveAd(data) {
+  return new Promise((resolve, reject) => {
+    let result,
+      url = 'http://likmap.org:8070/mobile/add-object',
+      cookies = 'SESSION=84877953-0c93-4dd8-82e2-509316b001ac',
+      // parsedUrl = urlModule.parse(url);
+      parsedUrl = {};
+      parsedUrl.headers = {
+        'Cookie': cookies,
+        'Content-Type': 'application/json',
+      };
+      parsedUrl.method = 'POST';
+      parsedUrl.uri = 'http://likmap.org:8070/mobile/add-object'
+      parsedUrl.body = {
+        'description': data.description,
+        'price': {
+          'price': data.price,
+          'currency': data.price,
+        },
+        'coordinates': {
+          'lat': data.lat,
+          'lng': data.lon,
+        },
+        'title': data.title
+      };
+      parsedUrl.json = true;
+    rp(parsedUrl, (error, response, body) => {
+      if (IsJsonString(body)) {
+          resolve(response);
+      } else {
+        reject(error);
+      }
+    });
+  });
 }
